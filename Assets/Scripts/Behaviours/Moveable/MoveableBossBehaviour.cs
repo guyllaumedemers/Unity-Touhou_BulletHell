c95ffe1189424120703eeefb3e513fb49823770a
Guyllaume Demers <guyllaume_demers@hotmail.fr>
2021-06-03 11:03:21 -0400

    Pushing comments on Movement Behaviour
    How do I handle movement for all unti types. Thinking about creating a waypoint system. Touhou game are not based on randomized values.

--- Assets/Scripts/Behaviours/Moveable/MoveableBossBehaviour.cs
+++ Assets/Scripts/Behaviours/Moveable/MoveableBossBehaviour.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class MoveableBossBehaviour : IMoveable
 {
-    public Vector3 Move(Vector3 pos, float angle, float speed) => Vector3.zero;
+    public Vector3 Move(Vector3 pos, float angle, float speed)
+    {
+        /*  How do I set the target position if the Vector3 pass as argument is the reference for my current position from which I update
+         *  Maybe I could create a Waypoint System?
+         *  
+         *  Units would be moving to these waypoints as well as the Boss
+         *  
+         *  I could than set the order in which the waypoints are set has target depending on the Unit Type
+         */
+        return Vector3.zero;
+    }
 }
