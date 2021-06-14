031b2b321b47c2bd6f3d3e2b612dbf79d538db58
Guyllaume Demers <guyllaume_demers@hotmail.fr>
2021-06-14 11:32:01 -0400

    Setting the CollisionSystem Update function to check the layer on which the bullet is on

--- Assets/Scripts/Collisions/CollisionSystem.cs
+++ Assets/Scripts/Collisions/CollisionSystem.cs
@@ -12,14 +12,28 @@ public class CollisionSystem : SingletonMono<CollisionSystem>, IFlow
     public bool DistanceCheck(Vector2 pos, Vector2 target, float rad) => Vector2.Distance(pos, target) <= rad;
 
     //// O(N^2) => R-tree might be the better solution for the lookup
-    private void UpdateCollisionSystem(Dictionary<string, HashSet<Bullet>> bulletsDict, Dictionary<string, HashSet<Unit>> unitsDict)
+    private void UpdateCollisionSystem(Dictionary<string, HashSet<Bullet>> bulletsDict, Dictionary<string, HashSet<Unit>> unitsDict, PlayerController player)
     {
-        foreach (var u in unitsDict.Keys.SelectMany(key => unitsDict[key]))
+        foreach (var b in bulletsDict.Keys.SelectMany(key => bulletsDict[key]))
         {
-            foreach (var b in bulletsDict.Keys.SelectMany(key => bulletsDict[key]).Where(x => DistanceCheck(u.transform.position, x.transform.position, u.rad)))
+            if (b.ignoredLayer != IgnoreLayerEnum.Player)
             {
-                u.TakeDamage(b.dmg);
-                products.Enqueue(b);
+                // do distance check with the player
+                if (DistanceCheck(b.transform.position, player.transform.position, player.rad))
+                {
+                    player.TakeDamage(b.dmg);
+                    products.Enqueue(b);
+                }
+            }
+            else
+            {
+                // do distance check with the units
+                foreach (var u in unitsDict.Keys.SelectMany(key => unitsDict[key]).Where(x =>
+                DistanceCheck(b.transform.position, x.transform.position, x.rad)))
+                {
+                    u.TakeDamage(b.dmg);
+                    products.Enqueue(b);
+                }
             }
         }
         while (products.Count > 0) (products.Dequeue() as Bullet).Pool();
@@ -31,5 +45,5 @@ public class CollisionSystem : SingletonMono<CollisionSystem>, IFlow
 
     public void InitializationMethod() { }
 
-    public void UpdateMethod() => UpdateCollisionSystem(BulletManager.Instance.BulletsDict, UnitManager.Instance.UnitsDict);
+    public void UpdateMethod() => UpdateCollisionSystem(BulletManager.Instance.BulletsDict, UnitManager.Instance.UnitsDict, PlayerController.Instance);
 }
