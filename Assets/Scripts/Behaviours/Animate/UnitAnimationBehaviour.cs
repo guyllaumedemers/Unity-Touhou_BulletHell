45b043c76caf7eec07f4149c15de6cf9855389ff
guyllaume demers <guyllaume_demers@hotmail.fr>
2021-06-27 10:38:30 -0400

    removing playerAnimation script, converting only to single UnitAnimation Script

--- Assets/Scripts/Behaviours/Animate/UnitAnimationBehaviour.cs
+++ Assets/Scripts/Behaviours/Animate/UnitAnimationBehaviour.cs
@@ -4,7 +4,7 @@ public class UnitAnimationBehaviour : IAnimate
 {
     public void Animate(Animator animator, SpriteRenderer spr, Vector2 inputs)
     {
-        spr.flipX = inputs.x < 0;                                                   // maybe not the best choice as it create more scripts just to handle this one difference
+        spr.flipX = inputs.x < 0;
         animator.SetFloat("vX", inputs.x);
         animator.SetFloat("vY", inputs.y);
     }
