ad3efc37c3bd4c177cf3236a8a4fad571d874b66
guyllaume demers <guyllaume_demers@hotmail.fr>
2021-07-25 10:21:11 -0400

    Pushing changes on Unit base class
    not abstract anymore and removed sub-classes. Will retrieve values from database in the future

--- Assets/Scripts/SQLite Database/DatabaseHandler.cs
+++ Assets/Scripts/SQLite Database/DatabaseHandler.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public static class DatabaseHandler
 {
-
+    public static void AddDatabaseEntry(string table, string type, params float[] values)
+    {
+        //TODO access database and add entry for the BulletData OR UnitData
+    }
 }
