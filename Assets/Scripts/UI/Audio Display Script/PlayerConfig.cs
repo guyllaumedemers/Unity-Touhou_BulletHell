9462d0d963f0555d51547d05f5596c0767c28336
guyllaume demers <guyllaume_demers@hotmail.fr>
2021-07-30 07:41:37 -0400

    Refactoring playerConfig script
    moved text management function for the Audio Setting inside the audio manager for clarity and added comments to the current problem with the loadPlayerPrefs function inside the AudioManager

--- Assets/Scripts/UI/Audio Display Script/PlayerConfig.cs
+++ Assets/Scripts/UI/Audio Display Script/PlayerConfig.cs
@@ -3,20 +3,6 @@ using UnityEngine;
 
 public static class PlayerConfig
 {
-    public static int IncrementVolume(TextMeshProUGUI volumeTxt, int value)
-    {
-        if (++value >= 100) value = 100;
-        volumeTxt.text = value.ToString() + "%";
-        return value;
-    }
-
-    public static int DecrementVolume(TextMeshProUGUI volumeTxt, int value)
-    {
-        if (--value <= 0) value = 0;
-        volumeTxt.text = value.ToString() + "%";
-        return value;
-    }
-
     public static void SetPlayerPref(float[] values, params string[] playerPrefEntries)
     {
         if (values.Length != playerPrefEntries.Length)
